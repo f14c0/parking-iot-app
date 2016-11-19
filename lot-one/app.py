88b156c0bc01c659f0e8afd78c946eee4c58c5ac
Julian Vega <julian.vega@theapescompany.com>
2016-11-19 14:00:03 -0500

    Name refactor

--- lot-one/app.py
+++ lot-one/app.py
@@ -1,67 +1,58 @@
-#required libraries
-import sys
-import ssl
 import paho.mqtt.client as mqtt
-import os
 import json
+import os
+import ssl
 
-#called while client tries to establish connection with the server
 def on_connect(mqttc, obj, flags, rc):
-    print "hola"
-    if rc==0:
-        print ("Subscriber Connection status code: "+str(rc)+" | Connection status: successful")
-    elif rc==1:
-        print ("Subscriber Connection status code: "+str(rc)+" | Connection status: Connection refused")
+    if rc == 0:
+        print ("Subscriber Connection status code: " + str(rc) + " | Connection status: successful")
+    elif rc == 1:
+        print ("Subscriber Connection status code: " + str(rc) + " | Connection status: Connection refused")
 
-#called when a topic is successfully subscribed to
+
 def on_subscribe(mqttc, obj, mid, granted_qos):
-    print("Subscribed: "+str(mid)+" "+str(granted_qos)+"data"+str(obj))
+    print("Subscribed: " + str(mid) + " " + str(granted_qos) + "data" + str(obj))
+    first_message()
 
-#called when a message is received by a topic
+
 def on_message(mqttc, obj, msg):
-    print("Received message from topic: "+msg.topic+" | QoS: "+str(msg.qos)+" | Data Received: "+str(msg.payload))
+    print(
+    "Received message from topic: " + msg.topic + " | QoS: " + str(msg.qos) + " | Data Received: " + str(msg.payload))
 
-#creating a client with client-id=mqtt-test
-mqttc = mqtt.Client(client_id="mqtt-test")
 
+def on_publish(client, userdata, mid):
+    print("Message is published")
+
+
+def first_message():
+    payload = json.dumps({
+        "state":{
+            "reported":{
+                "this_thing_is_alive":True,
+                "color":{
+                    "r":255,
+                    "g":1,
+                    "b":255
+                }
+            }
+        }
+    })
+    mqttc.publish("$aws/things/parking-lot-one/shadow/update", payload)
+
+
+root_cert_path = os.path.join(os.path.dirname(__file__), '../certs/root-CA.crt')
+cert_path = os.path.join(os.path.dirname(__file__), '../certs/one/69ecbfbb92-certificate.pem.crt')
+private_key_path = os.path.join(os.path.dirname(__file__), '../certs/one//69ecbfbb92-private.pem.key')
+
+
+mqttc = mqtt.Client(client_id="parking-lot-one")
 mqttc.on_connect = on_connect
 mqttc.on_subscribe = on_subscribe
 mqttc.on_message = on_message
+mqttc.on_publish = on_publish
 
-#Configure network encryption and authentication options. Enables SSL/TLS support.
-#adding client-side certificates and enabling tlsv1.2 support as required by aws-iot service
-root_cert_path  = os.path.join(os.path.dirname(__file__), '../certs/root-CA.crt')
-cert_path  = os.path.join(os.path.dirname(__file__), '../certs/868fde4221-certificate.pem.crt')
-private_key_path  = os.path.join(os.path.dirname(__file__), '../certs/868fde4221-private.pem.key')
+mqttc.tls_set(root_cert_path, certfile=cert_path, keyfile=private_key_path, tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
+mqttc.connect("a1io4zpp14aci0.iot.us-west-2.amazonaws.com", port=8883)  # AWS IoT service hostname and port
+mqttc.subscribe("$aws/things/parking-lot-one/shadow/update/#",  qos=1)  # The names of these topics start with $aws/things/thingName/shadow."
 
-#mqttc.tls_set("./certs/root-CA.crt",
-#                certfile="./certs/868fde4221-certificate.pem.key",
-#                keyfile="./certs/868fde4221-privete.pem.key",
-#              tls_version=ssl.PROTOCOL_TLSv1_2,
-#              ciphers=None)
-
-
-mqttc.tls_set(root_cert_path,certfile=cert_path, keyfile=private_key_path,tls_version=ssl.PROTOCOL_TLSv1_2, ciphers=None)
-
-
-#connecting to aws-account-specific-iot-endpoint
-print mqttc.connect("a1io4zpp14aci0.iot.us-west-2.amazonaws.com", port=8883) #AWS IoT service hostname and portno
-
-#the topic to publish to
-mqttc.subscribe("$aws/things/parking-lot-1/shadow/update", qos=1) #The names of these topics start with $aws/things/thingName/shadow."
-
-thing = "parking-lot-1"
-payload = json.dumps({
-    "state": {
-        "reported": {
-            "this_thing_is_alive": True
-        }
-    }
-})
-
-print mqttc.publish("$aws/things/" + thing +"/shadow/update", payload)
-
-
-
-#automatically handles reconnecting
 mqttc.loop_forever()
